2518fec8bd77a1e1a7987ca616d621db1673c5c1
Shekhar Roy <43708251+shekharroy5393@users.noreply.github.com>
2021-03-03 21:07:52 +0530

    Update Hamming Code with error Possibilities and Solving  in C.c

--- Hamming Code with error Possibilities and Solving  in C.c
+++ Hamming Code with error Possibilities and Solving  in C.c
@@ -2,31 +2,16 @@
 #include<math.h>
 #include<time.h>
 #include<stdlib.h>
+void Op();
 int main()
 {
 	int n,j,k,i=0,sum =0,maxparity = 10;
 	printf("Enter the no of elements : ");
 	scanf("%d",&n);
 	printf("%d\n",n);
-	int s[n],temp[100],temp2[100],t,t1,t2,t3;
-	printf("Enter only binary numbers :");
-	for(i=0;i<n;i++)
-    {
-		scanf("%d",&s[i]);
-	}
-	int MsgBit = (int)(sizeof(s)/sizeof(s[0]));
-	// printf("then massege type bits is %d\n",MsgBit);
-	for(i=0;i<=10;i++)
-    {
-		t1=MsgBit+i+1;
-		t2=pow(2,i);
-		if(t2>=t1)
-        {
-			t3=i;
-			goto lebel;
-		}
-	}
-	lebel : printf("number of Parity Bits is :%d \n",t3);
+	int s[n],temp[100],temp2[100],t,t1,t2,t3,MsgBit;
+	void Op(){
+	printf("number of Parity Bits is :%d \n",t3);
 	int t4 = t3 + MsgBit;
     printf("Number of Hamming code is : %d\n",t4);
 	// char Array[t4];
@@ -129,21 +114,39 @@ int main()
         printf("At Position %d: %d\n",t+1,b[m]);
         m--;
     }
-    int position = 0;
+    int POS = 0;
      for(m=0;m<t3;m++)
     {
-        position=position+b[t3-m-1]*pow(2,m);
+        POS=POS+b[t3-m-1]*pow(2,m);
     }
-    printf("Position of Error: %d\n",position);
-    if(temp2[position-1]==0)
-        temp2[position-1]=1;
+    printf("Position of Error: %d\n",POS);
+    if(temp2[POS-1]==0)
+        temp2[POS-1]=1;
     else
-        temp2[position-1]=0;
+        temp2[POS-1]=0;
 
     printf("\nError Correction complete :  ");
     for(i=0;i<t4;i++)
     {
         printf("%d ",temp2[i]);
     }
+}
+	printf("Enter only binary numbers :");
+	for(i=0;i<n;i++)
+    {
+		scanf("%d",&s[i]);
+	}
+	MsgBit = (int)(sizeof(s)/sizeof(s[0]));
+	// printf("then massege type bits is %d\n",MsgBit);
+	for(i=0;i<=7;i++)
+    {
+		t1=MsgBit+i+1;
+		t2=pow(2,i);
+		if(t2>=t1)
+        {
+			t3=i;
+			Op();
+		}
+	}
 	return 0;
 }
